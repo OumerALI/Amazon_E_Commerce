aff81966f523ec3f6c1545e581b8f86cac3fddfb
Oumer <oumerali525@gmail.com>
2024-11-11 03:34:12 +0300

    header added

---
+++ amazon_clone/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+import Carousel from "./components/Carousel/CarouselEffect";
+function App() {
+  return (
+    <>
+      <Header />
+      <Carousel />
+    </>
+  );
+}
+
+export default App;
